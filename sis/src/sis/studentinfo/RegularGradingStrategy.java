06d8b15ca22c4e84899e4ce04bf37f1a07507aec
javajigi <javajigi@gmail.com>
2013-04-11 12:16:31 +0900

    6장 예제 시작

--- sis/src/sis/studentinfo/RegularGradingStrategy.java
+++ sis/src/sis/studentinfo/RegularGradingStrategy.java
@@ -3,16 +3,13 @@ package sis.studentinfo;
 import sis.studentinfo.Student.Grade;
 
 public class RegularGradingStrategy implements GradingStrategy {
-	@Override
 	public int gradePointsFor(Grade grade) {
-		if (grade == Grade.A)
-			return 4;
-		else if (grade == Grade.B)
-			return 3;
-		else if (grade == Grade.C)
-			return 2;
-		else if (grade == Grade.D)
-			return 1;
-		return 0;
+		switch(grade) {
+			case A: return 4;
+			case B: return 3;
+			case C: return 2;
+			case D: return 1;
+			default: return 0;
+		}
 	}
 }
