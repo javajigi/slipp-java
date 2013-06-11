1ae16b8e97f4bf91b29491b5624155e9a8a35b83
javajigi <javajigi@gmail.com>
2013-06-11 12:29:09 +0900

    7장 두번째 예제 추가함.

---
+++ sis/src/sis/studentinfo/Performance.java
@@ -0,0 +1,21 @@
+package sis.studentinfo;
+
+public class Performance {
+	private int[] tests;
+	
+	public void setNumerOfTests(int numberOfTests) {
+		this.tests = new int[numberOfTests];
+	}
+
+	public void set(int testNumber, int score) {
+		
+	}
+	
+	public int get(int testNumber) {
+		return 0;
+	}
+
+	public double average() {
+		return 0;
+	}
+}
---
+++ sis/test/sis/studentinfo/PerformanceTest.java
@@ -0,0 +1,20 @@
+package sis.studentinfo;
+
+import junit.framework.TestCase;
+
+public class PerformanceTest extends TestCase {
+	private static final double tolerance = 0.005;
+	
+	public void testAverage() {
+		Performance performance = new Performance();
+		performance.setNumerOfTests(4);
+		performance.set(0, 98);
+		performance.set(1, 92);
+		performance.set(2, 81);
+		performance.set(3, 72);
+		
+		assertEquals(92, performance.get(1));
+		
+		assertEquals(85.75, performance.average(), tolerance);
+	}
+}
