74da49feb6d9a79b414be6cf102b4b3ec7333890
javajigi <javajigi@gmail.com>
2013-06-28 15:48:10 +0900

    chapter 3-4번 연습문제

--- chessgame/src/chess/Board.java
+++ chessgame/src/chess/Board.java
@@ -6,6 +6,11 @@ import pieces.Pawn;
 
 class Board {
 	ArrayList<Pawn> pawns = new ArrayList<Pawn>();
+	
+	Board() {
+		addPawn(new Pawn(Pawn.COLOR_WHITE, Pawn.SYMBOL_LOWERCASE_PAWN));
+		addPawn(new Pawn(Pawn.COLOR_BLACK, Pawn.SYMBOL_UPPERCASE_PAWN));
+	}
 
 	void addPawn(Pawn pawn) {
 		pawns.add(pawn);
--- chessgame/test/chess/BoardTest.java
+++ chessgame/test/chess/BoardTest.java
@@ -8,16 +8,16 @@ import pieces.Pawn;
 public class BoardTest extends TestCase {
 	public void testCreate() throws Exception {
 		Pawn whitePawn = new Pawn(Pawn.COLOR_WHITE, 'p');
-		
 		Board board = new Board();
+		assertEquals(2, board.countPawnSize());
 		board.addPawn(whitePawn);
-		assertEquals(1, board.countPawnSize());
+		assertEquals(3, board.countPawnSize());
 		ArrayList<Pawn> pawns = board.getPawns();
 		assertTrue(pawns.contains(whitePawn));
 		
 		Pawn blackPawn = new Pawn(Pawn.COLOR_BLACK, 'P');
 		board.addPawn(blackPawn);
-		assertEquals(2, board.countPawnSize());
+		assertEquals(4, board.countPawnSize());
 		pawns = board.getPawns();
 		assertTrue(pawns.contains(whitePawn));
 		assertTrue(pawns.contains(blackPawn));
