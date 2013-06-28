dc68eff2ccafb070bf610e33a58325de3b754725
javajigi <javajigi@gmail.com>
2013-06-28 15:28:54 +0900

    chapter 2-4번 연습문제

--- chessgame/src/Board.java
+++ chessgame/src/Board.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
+
 
 class Board {
+	ArrayList<Pawn> pawns = new ArrayList<Pawn>();
+
+	void addPawn(Pawn pawn) {
+		pawns.add(pawn);
+	}
+
+	int countPawnSize() {
+		return pawns.size();
+	}
+
+	ArrayList<Pawn> getPawns() {
+		return pawns;
+	}
 
 }
--- chessgame/test/BoardTest.java
+++ chessgame/test/BoardTest.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
 
 public class BoardTest extends TestCase {
 	public void testCreate() throws Exception {
-		new Board();
+		Pawn whitePawn = new Pawn(Pawn.COLOR_WHITE);
+		
+		Board board = new Board();
+		board.addPawn(whitePawn);
+		assertEquals(1, board.countPawnSize());
+		ArrayList<Pawn> pawns = board.getPawns();
+		assertTrue(pawns.contains(whitePawn));
+		
+		Pawn blackPawn = new Pawn(Pawn.COLOR_BLACK);
+		board.addPawn(blackPawn);
+		assertEquals(2, board.countPawnSize());
+		pawns = board.getPawns();
+		assertTrue(pawns.contains(whitePawn));
+		assertTrue(pawns.contains(blackPawn));		
 	}
 }
