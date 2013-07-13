42fb348961026535ecdbf16f6c5a11f75eb9a231
javajigi <javajigi@gmail.com>
2013-07-13 16:05:28 +0900

    이동할 index를 찾는 메소드를 하나로 통일함.

--- java-api/src/net/slipp/sort/Sort.java
+++ java-api/src/net/slipp/sort/Sort.java
@@ -6,34 +6,31 @@ import java.util.Collections;
 public class Sort {
 	static void sortAsc(ArrayList<Person> persons) {
 		for (int i = 0; i < persons.size() - 1; i++) {
-			int minimunIndex = findMinimunIndex(persons, i);
-			Collections.swap(persons, i, minimunIndex);
+			int targetIndex = findTargetIndex(persons, i, true);
+			Collections.swap(persons, i, targetIndex);
 		}
 	}
 	
-	private static int findMinimunIndex(ArrayList<Person> values, int currentIndex) {
-		int minimunIndex = currentIndex;
+	private static int findTargetIndex(ArrayList<Person> values, int currentIndex, boolean isAsc) {
+		int targetIndex = currentIndex;
 		for (int j = currentIndex+1; j < values.size(); j++) {
-			if (values.get(j).isYounger(values.get(minimunIndex)) ) {
-				minimunIndex = j;
+			boolean isYounger = values.get(j).isYounger(values.get(targetIndex));
+			if (isAsc) {
+				if (isYounger) {
+					targetIndex = j;
+				}
+			} else {
+				if (!isYounger) {
+					targetIndex = j;
+				}
 			}
 		}
-		return minimunIndex;
-	}
-	
-	private static int findMaximumIndexIndex(ArrayList<Person> values, int currentIndex) {
-		int maximumIndex = currentIndex;
-		for (int j = currentIndex+1; j < values.size(); j++) {
-			if (!values.get(j).isYounger(values.get(maximumIndex)) ) {
-				maximumIndex = j;
-			}
-		}
-		return maximumIndex;
+		return targetIndex;
 	}
 	
 	static void sortDesc(ArrayList<Person> persons) {
 		for (int i = 0; i < persons.size() - 1; i++) {
-			int maximumIndex = findMaximumIndexIndex(persons, i);
+			int maximumIndex = findTargetIndex(persons, i, false);
 			Collections.swap(persons, i, maximumIndex);
 		}		
 	}
