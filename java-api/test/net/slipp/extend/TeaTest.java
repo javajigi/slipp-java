f0f52f3776c0dc3a1571fa3e0827aa5c2c4e05d5
javajigi <javajigi@gmail.com>
2013-07-15 16:16:27 +0900

    starbuzz 상속 1단계

---
+++ java-api/test/net/slipp/extend/TeaTest.java
@@ -0,0 +1,10 @@
+package net.slipp.extend;
+
+import junit.framework.TestCase;
+
+public class TeaTest extends TestCase {
+	public void testPrepareRecipe() throws Exception {
+		Tea tea = new Tea();
+		tea.prepareRecipe();
+	}
+}
