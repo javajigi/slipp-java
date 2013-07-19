32c4ea37e9248bcd1d66e4c63c5df3edaf51cc4f
javajigi <javajigi@gmail.com>
2013-07-19 20:42:18 +0900

    인자에 다형성 추가 예제

---
+++ java-api/test/net/slipp/polymorphism/DrawShapeTest.java
@@ -0,0 +1,12 @@
+package net.slipp.polymorphism;
+
+import junit.framework.TestCase;
+
+public class DrawShapeTest extends TestCase {
+	public void testDrawShape() {
+		DrawShape shape = new DrawShape();
+//		assertEquals("draw Circle", shape.drawShape(new Circle()));
+//		assertEquals("draw Triangle", shape.drawShape(new Triangle()));
+//		assertEquals("draw Rectangle", shape.drawShape(new Rectangle()));
+	}
+}
--- java-api/test/net/slipp/polymorphism/ShapeTest.java
+++ java-api/test/net/slipp/polymorphism/ShapeTest.java
@@ -3,14 +3,14 @@ package net.slipp.polymorphism;
 import junit.framework.TestCase;
 
 public class ShapeTest extends TestCase {
-	public void testShape(){
-	    Shape circle = new Circle();
-	    assertEquals("draw Circle", circle.draw());
+	public void testShape() {
+		Shape circle = new Circle();
+		assertEquals("draw Circle", circle.draw());
 
-	    Shape rectangle = new Rectangle();
-	    assertEquals("draw Rectangle", rectangle.draw());
+		Shape rectangle = new Rectangle();
+		assertEquals("draw Rectangle", rectangle.draw());
 
-	    Shape triangle = new Triangle();
-	    assertEquals("draw Triangle", triangle.draw());
+		Shape triangle = new Triangle();
+		assertEquals("draw Triangle", triangle.draw());
 	}
 }
