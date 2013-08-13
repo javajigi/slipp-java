152042c55bba162660d20221f0e2f57a36c9ca52
javajigi <javajigi@gmail.com>
2013-08-13 18:38:44 +0900

    웹 서버 예제 초기화함.

--- java-api/src/net/slipp/webserver/WebServer.java
+++ java-api/src/net/slipp/webserver/WebServer.java
@@ -9,19 +9,15 @@ public class WebServer {
 	private final static Logger log = Logger.getLogger(WebServer.class.getName());
 	
     public static void main(String argv[]) throws Exception {
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 80번 포트를 사용한다.
-        ServerSocket listenSocket = new ServerSocket(80);
+        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+        ServerSocket listenSocket = new ServerSocket(8080);
         log.log(Level.INFO, "WebServer Socket Created");
 
         // 클라이언트가 연결될때까지 대기한다.
-        try {
-		    Socket connection;
-		    while ((connection = listenSocket.accept()) != null) {
-		    	RequestHandler requestHandler = new RequestHandler(connection);
-		        requestHandler.start();
-		    }
-        }  finally {
-        	listenSocket.close();
+        Socket connection;
+        while ((connection = listenSocket.accept()) != null) {
+        	RequestHandler requestHandler = new RequestHandler(connection);
+            requestHandler.start();
         }
     }
 }
