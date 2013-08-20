ea4cea480ace290e41e02de581f9e0b06facb340
javajigi <javajigi@gmail.com>
2013-08-20 11:22:36 +0900

    파일 검증하는 부분 추가함.

--- java-api/test/net/slipp/reflection/GetterSetterGeneratorTest.java
+++ java-api/test/net/slipp/reflection/GetterSetterGeneratorTest.java
@@ -1,5 +1,9 @@
 package net.slipp.reflection;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import org.junit.Test;
 
 public class GetterSetterGeneratorTest {
@@ -7,5 +11,7 @@ public class GetterSetterGeneratorTest {
 	public void generate() throws Exception {
 		Class<Student> clazz = Student.class;
 		
+		File file = new File("resources/Student.java");
+		assertTrue(file.exists());
 	}
 }
